ebb68f14f6e38a8a30e7541d816c44e1c9bb7531
ktt45678 <ktt45678@gmail.com>
2020-12-12 20:08:14 +0700

    Enable cors middleware

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
 const express = require('express');
 const routes = require('./routes');
+const cors = require('./src/middlewares/cors');
 const db = require('./src/services/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Set up middleware for enabling cors
+app.use(cors);
 
 // Set up middleware for request parsing
 app.use(express.json());
