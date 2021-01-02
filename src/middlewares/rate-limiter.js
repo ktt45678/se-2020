ee35760a1272de8d31f2a61d386523ae17cbc0bf
ktt45678 <ktt45678@gmail.com>
2021-01-02 14:29:25 +0700

    Add missing lines

--- src/middlewares/rate-limiter.js
+++ src/middlewares/rate-limiter.js
@@ -7,13 +7,24 @@ module.exports = (expiry = 120) => {
       req.currentUser = { _id: req.ip }
     }
     const key = JSON.stringify({ user: req.currentUser._id, path: req.path, method: req.method });
-    const ttl = await redisService.ttl(key);
-    if (ttl >= 0) {
-      return res.status(429).send({ ttl, message: `You can request again in ${ttl} seconds` });
+    try {
+      const ttl = await redisService.ttl(key);
+      if (ttl >= 0) {
+        return res.status(429).send({ ttl, message: `You can request again in ${ttl} seconds` });
+      }
+    } catch (e) {
+      console.error(e);
+      return res.status(500).send({ error: 'Internal server error' });
     }
+    res.on('finish', async () => {
+      if (res.statusCode >= 200 && res.statusCode <= 299) {
+        try {
+          await redisService.set(key, expiry, true);
+        } catch (e) {
+          console.error(e);
+        }
+      }
+    });
     next();
-    if (res.statusCode >= 200 && res.statusCode <= 299) {
-      await redisService.set(key, expiry, true);
-    }
   }
 }
